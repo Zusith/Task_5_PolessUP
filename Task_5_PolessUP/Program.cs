a257dd7f3080ddc7183d06ed4f97195ebf47612b
Zusith <haifi2@mail.ru>
2023-04-03 12:06:58 +0300

    Добавьте файлы проекта.

---
+++ Task_5_PolessUP/Program.cs
@@ -0,0 +1,13 @@
+﻿int[] nums = new int[] {2, 3, 1};
+Console.WriteLine("Ввод: [" + string.Join("; ", nums) + "]");
+
+Console.WriteLine("Вывод: " + IndexMaxElementOnGroup(nums));
+Console.ReadLine();
+
+int IndexMaxElementOnGroup(int[] numbers)
+{
+    int[] nums = new int[numbers.Length];
+    Array.Copy(numbers, nums, numbers.Length);
+    Array.Sort(nums);
+    return Array.IndexOf(numbers, nums[^1]);
+}
